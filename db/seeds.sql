856a3d037d2e87ea8131d2869cca824817b32038
Ogonnaya <hello@ogonnaya.com>
2019-12-15 16:10:42 -0600

    added all files and folders

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Flashback', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Regular', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Chillin n Grillin', true);
+
