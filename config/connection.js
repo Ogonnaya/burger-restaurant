8dfb35d651412c349d7c6f9d6d608f0fae5b376c
Ogonnaya <hello@ogonnaya.com>
2020-01-26 18:39:38 -0600

    updated database info

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,13 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1234567890",
-  database: "burgers_db"
+  //   host: "localhost",
+  //   port: 3306,
+  //   user: "root",
+  //   password: "1234567890",
+  //   database: "burgers_db"
+  use_env_variable: "JAWSDB_URL",
+  dialect: "mysql"
 });
 
 // Make connection.
